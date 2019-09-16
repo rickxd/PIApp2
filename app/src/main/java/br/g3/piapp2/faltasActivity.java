a67a487edea09b46d4c734f6e68c7a6187dfef67
Henrique <rick.bomfim@hotmaill.com>
2019-09-16 11:33:44 -0300

    Algumas telas a mais, nenhuma delas funcional ainda.

---
+++ app/src/main/java/br/g3/piapp2/faltasActivity.java
@@ -0,0 +1,14 @@
+package br.g3.piapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class faltasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faltas);
+    }
+}
